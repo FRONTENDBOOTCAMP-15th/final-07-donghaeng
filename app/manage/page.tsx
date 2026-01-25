34719797c7451dc3e2424495ad43f6a8cf71365c
minggichae <che08180@naver.com>
2026-01-25 23:10:19 +0900

    style: 참여자 관리 디자인 작업 중 중간 커밋 #16

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,28 +1,34 @@
 import profile from '@/public/icon/profile.svg';
 import heart from '@/public/icon/heart.svg';
+import styles from './manage.module.css';
 
 export default function Manage() {
   return (
     <>
-      <h1>신청자 목록</h1>
-      <div>
-        <img src={profile.src} alt="프로필이미지" />
-        <div>
-          <span>나폴리맛피자</span>
-          <figure>
-            <img src={heart.src} alt="심박수이미지" />
-            <figcaption>90bpm</figcaption>
-          </figure>
-          <button>승인</button>
-          <button>거절</button>
-        </div>
-        <div>
-          <h3>질문1</h3>
-          <p>질문에 대한 답변을 가져오는 위치</p>
-        </div>
-        <div>
-          <h3>질문2</h3>
-          <p>질문에 대한 답변을 가져오는 위치</p>
+      <div className={styles['manage-body']}>
+        <h1 className={styles['manage-title']}>신청자 목록</h1>
+        <div className={styles['applicant-div']}>
+          <div className={styles['applicant-information']}>
+            <img src={profile.src} alt="프로필이미지" />
+            <span>나폴리맛피자</span>
+            <figure>
+              <img src={heart.src} alt="심박수이미지" />
+              <figcaption>
+                90 <br />
+                bpm
+              </figcaption>
+            </figure>
+            <button className={styles['btn-approve']}>승인</button>
+            <button className={styles['btn-no']}>거절</button>
+          </div>
+          <div className={styles['question-div']}>
+            <dt>질문1</dt>
+            <dd>질문에 대한 답변을 가져오는 위치</dd>
+          </div>
+          <div className={styles['question-div']}>
+            <dt>질문2</dt>
+            <dd>질문에 대한 답변을 가져오는 위치</dd>
+          </div>
         </div>
       </div>
     </>
