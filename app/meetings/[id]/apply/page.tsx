345d7f4a377113c2079c66cb9bd85f81cda752bd
minggichae <che08180@naver.com>
2026-01-25 22:42:43 +0900

    style: 모임 신청 디자인 작업 중 중간 커밋 #17

--- app/meetings/[id]/apply/page.tsx
+++ app/meetings/[id]/apply/page.tsx
@@ -1,19 +1,22 @@
+import styles from './apply.module.css';
+
 export default function Apply() {
   return (
     <>
-      <h1>모임 신청</h1>
-      <div>
-        <h3>질문 1</h3>
-        <textarea name="질문 1" id="" style={{ backgroundColor: '#c2c2c2' }}></textarea>
-      </div>
-      <div>
-        <h3>질문 2</h3>
-        <textarea name="질문 2" id="" style={{ backgroundColor: '#c2c2c2' }}></textarea>
-      </div>
-      <div>
-        <button>신청하기</button>
-        <br />
-        <button>취소하기</button>
+      <div className={styles['apply-body']}>
+        <h1 className={styles['apply-title']}>모임 신청</h1>
+        <div className={styles['question-div']}>
+          <h3>질문 1</h3>
+          <textarea name="질문 1" id=""></textarea>
+        </div>
+        <div className={styles['question-div']}>
+          <h3>질문 2</h3>
+          <textarea name="질문 2" id=""></textarea>
+        </div>
+        <div className={styles['btn-div']}>
+          <button className={styles['btn-apply']}>신청하기</button>
+          <button className={styles['btn-cancel']}>취소하기</button>
+        </div>
       </div>
     </>
   );
