2a498ed0e876bf517a6b9fd20f42ec9dd6410b4a
minggichae <che08180@naver.com>
2026-01-26 11:12:49 +0900

    style: 모임 신청 디자인 및 반응형 작업 완료 #17

--- app/meetings/[id]/apply/page.tsx
+++ app/meetings/[id]/apply/page.tsx
@@ -1,9 +1,9 @@
-import styles from './apply.module.css';
+import styles from './Apply.module.css';
 
 export default function Apply() {
   return (
     <>
-      <div className={styles['apply-body']}>
+      <form className={styles['apply-body']}>
         <h1 className={styles['apply-title']}>모임 신청</h1>
         <div className={styles['question-div']}>
           <h3>질문 1</h3>
@@ -14,10 +14,14 @@ export default function Apply() {
           <textarea name="질문 2" id=""></textarea>
         </div>
         <div className={styles['btn-div']}>
-          <button className={styles['btn-apply']}>신청하기</button>
-          <button className={styles['btn-cancel']}>취소하기</button>
+          <button type="submit" className={styles['btn-apply']}>
+            신청하기
+          </button>
+          <button type="button" className={styles['btn-cancel']}>
+            취소하기
+          </button>
         </div>
-      </div>
+      </form>
     </>
   );
 }
