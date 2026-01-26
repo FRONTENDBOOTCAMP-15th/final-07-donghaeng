e0c1af30b601751345e39a273f8f92668a3c1f04
jian526 <catthsudla@gmail.com>
2026-01-26 15:26:47 +0900

    fix: 모임등록 페이지 마크업 수정

--- app/meetings/add/page.tsx
+++ app/meetings/add/page.tsx
@@ -1,19 +1,23 @@
+import style from './create.module.css';
+import Header from './../../components/Header';
+
 export default function Add() {
   return (
     <>
-      <div className="add-wrap">
-        <form className="meetings-create">
+      <Header />
+      <div className={style['add-wrap']}>
+        <form className={style['meetings-create']}>
           <div className="meetings-add">
             <h2>모임 등록</h2>
-            <fieldset className="title-fieldset">
+            <fieldset className={style['title-fieldset']}>
               <label htmlFor="meetings-title">모임 제목</label>
               <input type="text" id="meetings-title" name="meetings-title" />
             </fieldset>
 
-            <fieldset className="category-fieldset">
+            <fieldset className={style['category-fieldset']}>
               <label htmlFor="category">카테고리</label>
-              <select name="category" id="category" required>
-                <option value="" disabled selected>
+              <select name="category" id="category" required defaultValue="">
+                <option value="" disabled>
                   선택
                 </option>
                 <option value="exercise">운동</option>
@@ -37,15 +41,15 @@ export default function Add() {
               </select>
             </fieldset>
 
-            <fieldset className="context-fieldset">
+            <fieldset className={style['context-fieldset']}>
               <label htmlFor="meetings-content">모임 설명</label>
               <input type="text" id="meetings-content" name="meetings-content" />
             </fieldset>
 
-            <fieldset className="img-fieldset">
+            <fieldset className={style['img-fieldset']}>
               <label htmlFor="meetings-img-label">모임 이미지</label>
               <label htmlFor="meetings-img" className="image-box">
-                <div className="ractingle">
+                <div className={style['ractingle']}>
                   <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                       d="M10.2857 1.28571C10.2857 0.574554 9.71116 0 9 0C8.28884 0 7.71429 0.574554 7.71429 1.28571V7.71429H1.28571C0.574554 7.71429 0 8.28884 0 9C0 9.71116 0.574554 10.2857 1.28571 10.2857H7.71429V16.7143C7.71429 17.4254 8.28884 18 9 18C9.71116 18 10.2857 17.4254 10.2857 16.7143V10.2857H16.7143C17.4254 10.2857 18 9.71116 18 9C18 8.28884 17.4254 7.71429 16.7143 7.71429H10.2857V1.28571Z"
@@ -57,17 +61,17 @@ export default function Add() {
               <input type="file" id="meetings-img" name="meetings-img" accept="image/*" hidden />
             </fieldset>
 
-            <fieldset className="tag-fieldset">
+            <fieldset className={style['tag-fieldset']}>
               <label htmlFor="tag">모임 태그</label>
               <input type="text" name="tag" id="tag" />
             </fieldset>
 
-            <fieldset className="region-fieldset">
+            <fieldset className={style['region-fieldset']}>
               <label htmlFor="region">지역</label>
               <button type="button">&plus; 추가</button>
             </fieldset>
 
-            <fieldset className="gender-fieldset">
+            <fieldset className={style['gender-fieldset']}>
               <label htmlFor="gender">성별</label>
               <select required id="gender" name="gender">
                 <option value="" disabled selected></option>
@@ -76,7 +80,7 @@ export default function Add() {
               </select>
             </fieldset>
 
-            <fieldset className="age-fieldset">
+            <fieldset className={style['age-fieldset']}>
               <label htmlFor="age">나이</label>
               <select required id="age" name="age">
                 <option value="" disabled selected></option>
@@ -86,20 +90,20 @@ export default function Add() {
                 <option value="forties_plus">40대 이상</option>
               </select>
             </fieldset>
-            <fieldset className="count">
+            <fieldset className={style['count']}>
               <label htmlFor="count-input">
                 인원
                 <br />
                 (0~300)명
               </label>
-              <div className="counter-wrapper">
+              <div className={style['counter-wrapper']}>
                 <button type="button" className="count-btn descrase">
                   <svg width="18" height="3" viewBox="0 0 18 3" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 1.5C0 0.670312 0.574554 0 1.28571 0H16.7143C17.4254 0 18 0.670312 18 1.5C18 2.32969 17.4254 3 16.7143 3H1.28571C0.574554 3 0 2.32969 0 1.5Z" fill="#323577" />
                   </svg>
                 </button>
                 <input type="number" id="count-input" name="count-input" min="0" max="300" value="10" />
-                <button type="button" className="count-btn increase">
+                <button type="button" className={style['count-btn increase']}>
                   <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
                       d="M10.2857 1.28571C10.2857 0.574554 9.71116 0 9 0C8.28884 0 7.71429 0.574554 7.71429 1.28571V7.71429H1.28571C0.574554 7.71429 0 8.28884 0 9C0 9.71116 0.574554 10.2857 1.28571 10.2857H7.71429V16.7143C7.71429 17.4254 8.28884 18 9 18C9.71116 18 10.2857 17.4254 10.2857 16.7143V10.2857H16.7143C17.4254 10.2857 18 9.71116 18 9C18 8.28884 17.4254 7.71429 16.7143 7.71429H10.2857V1.28571Z"
@@ -111,14 +115,14 @@ export default function Add() {
             </fieldset>
           </div>
 
-          <div className="question">
+          <div className={style['question']}>
             <h2>신청자 전용 질문 작성</h2>
             <fieldset className="question-1-field">
               <label htmlFor="question-1">1번 질문</label>
               <input type="text" name="question-1" id="question-1" placeholder="신청자에게 물어볼 질문을 작성하세요" />
             </fieldset>
 
-            <fieldset className="question-2-field">
+            <fieldset className={style['question-2-field']}>
               <label htmlFor="question-2">2번 질문</label>
               <input type="text" name="question-2" id="question-2" placeholder="신청자에게 물어볼 질문을 작성하세요" />
             </fieldset>
