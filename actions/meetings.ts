af3e91700d18da58105d35c845de7ec35afedfd5
holyhw <hyunookoo22@gmail.com>
2026-01-29 11:28:23 +0900

    feat: 모임등록, 수정, 삭제 api 설정

--- actions/meetings.ts
+++ actions/meetings.ts
@@ -54,3 +54,133 @@ export async function createApply(prevState: ActionState, formData: FormData): P
     return data; // 에러 응답 객체 반환
   }
 }
+
+/**
+ * 모임 등록
+ * @param {ActionState} prevState - 이전 상태(사용하지 않음)
+ * @param {FormData} formData - 모임 등록 정보를 담은 FormData 객체
+ * @returns {Promise<ActionState>} - 생성 결과 응답 객체
+ * @throws {Error} - 네트워크 오류 발생 시
+ * @description
+ * 새로운 모임을 등록하고, 성공 시 모임 목록으로 리다이렉트
+ * 실패 시 에러 메시지를 반환
+ */
+export async function createMeeting(prevState: ActionState, formData: FormData): Promise<ActionState> {
+  const accessToken = formData.get('accessToken');
+  formData.delete('accessToken');
+
+  // FormData를 일반 Object로 변환
+  const body = Object.fromEntries(formData.entries());
+
+  let res: Response;
+  let data: MeetingsInfoRes | ErrorRes;
+
+  try {
+    res = await fetch(`${API_URL}/seller/products`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(body),
+    });
+
+    data = await res.json();
+  } catch (error) {
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제로 모임 등록에 실패했습니다.' };
+  }
+
+  if (data.ok) {
+    redirect(`/meetings`); // 모임 목록 페이지로 리다이렉트
+  } else {
+    return data; // 에러 응답 객체 반환
+  }
+}
+
+/**
+ * 모임 수정
+ * @param {ActionState} prevState - 이전 상태(사용하지 않음)
+ * @param {FormData} formData - 모임 수정 정보를 담은 FormData 객체
+ * @returns {Promise<ActionState>} - 수정 결과 응답 객체
+ * @throws {Error} - 네트워크 오류 발생 시
+ * @description
+ * 기존 모임을 수정하고, 성공 시 모임 목록으로 리다이렉트
+ * 실패 시 에러 메시지를 반환
+ */
+export async function updateMeeting(prevState: ActionState, formData: FormData): Promise<ActionState> {
+  const accessToken = formData.get('accessToken');
+  const _id = formData.get('_id');
+  formData.delete('accessToken');
+  formData.delete('_id');
+
+  // FormData를 일반 Object로 변환
+  const body = Object.fromEntries(formData.entries());
+
+  let res: Response;
+  let data: MeetingsInfoRes | ErrorRes;
+
+  try {
+    res = await fetch(`${API_URL}/seller/products/${_id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(body),
+    });
+
+    data = await res.json();
+  } catch (error) {
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제로 모임 수정에 실패했습니다.' };
+  }
+
+  if (data.ok) {
+    redirect(`/meetings`); // 모임 목록 페이지로 리다이렉트
+  } else {
+    return data; // 에러 응답 객체 반환
+  }
+}
+
+/**
+ * 모임 삭제
+ * @param {ActionState} prevState - 이전 상태(사용하지 않음)
+ * @param {FormData} formData - 모임 삭제 정보를 담은 FormData 객체
+ * @returns {Promise<ActionState>} - 삭제 결과 응답 객체
+ * @throws {Error} - 네트워크 오류 발생 시
+ * @description
+ * 모임을 삭제하고, 성공 시 모임 목록으로 리다이렉트
+ * 실패 시 에러 메시지를 반환
+ */
+export async function deleteMeeting(prevState: ActionState, formData: FormData): Promise<ActionState> {
+  const accessToken = formData.get('accessToken');
+  const _id = formData.get('_id');
+
+  let res: Response;
+  let data: MeetingsInfoRes | ErrorRes;
+
+  try {
+    res = await fetch(`${API_URL}/seller/products/${_id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Client-Id': CLIENT_ID,
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    data = await res.json();
+  } catch (error) {
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제로 모임 삭제에 실패했습니다.' };
+  }
+
+  if (data.ok) {
+    redirect(`/meetings`); // 모임 목록 페이지로 리다이렉트
+  } else {
+    return data; // 에러 응답 객체 반환
+  }
+}
